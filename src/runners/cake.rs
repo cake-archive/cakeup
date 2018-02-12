562839703e0387a906c77424384f3a2aac0b1d88
Patrik Svensson <patrik@patriksvensson.se>
2018-02-12 11:59:00 +0100

    Respect exit codes.

--- src/runners/cake.rs
+++ src/runners/cake.rs
@@ -2,9 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-use std::io::{Error};
+use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
-use std::process;
+use std::process::{Command, ExitStatus};
 use semver::Version;
 use config::*;
 use utils::*;
@@ -12,12 +12,11 @@ use utils::*;
 pub struct Cake {
     pub path: PathBuf,
     pub version: Version,
-    pub host: Host
+    pub host: Host,
 }
 
 impl Cake {
     pub fn bootstrap(&self, config: &Config) -> Result<(), Error> {
-
         // Is bootstrapping supported?
         if self.version < Version::parse("0.24.0").unwrap() {
             println!("Warning: Bootstrapping requires at lest verison 0.24.0 of Cake.");
@@ -26,9 +25,10 @@ impl Cake {
 
         println!("Bootstrapping...");
 
+        let result: ExitStatus;
         match self.host {
             Host::Clr => {
-                process::Command::new(&self.path)
+                result = Command::new(&self.path)
                     .arg("--bootstrap")
                     .args(&config.remaining)
                     .status()?;
@@ -38,24 +38,29 @@ impl Cake {
                 if self.host == Host::Mono {
                     host = "mono";
                 }
-
-                process::Command::new(host)
+                result = Command::new(host)
                     .arg(&self.path)
                     .arg("--bootstrap")
                     .args(&config.remaining)
                     .status()?;
             }
         };
+
+        if result.code() != Some(0) {
+            return Err(Error::new(ErrorKind::Other,
+                format!("Exit code was {}.", result.code().unwrap())))
+        }
+
         return Ok(());
     }
 
     pub fn execute(&self, config: &Config) -> Result<(), Error> {
-
         println!("Executing...");
 
+        let result: ExitStatus;
         match self.host {
             Host::Clr => {
-                process::Command::new(&self.path)
+                result = Command::new(&self.path)
                     .args(&config.remaining)
                     .status()?;
             }
@@ -64,13 +69,18 @@ impl Cake {
                 if self.host == Host::Mono {
                     host = "mono";
                 }
-
-                process::Command::new(host)
+                result = Command::new(host)
                     .arg(&self.path)
                     .args(&config.remaining)
                     .status()?;
             }
         };
+
+        if result.code() != Some(0) {
+            return Err(Error::new(ErrorKind::Other,
+                format!("Exit code was {}.", result.code().unwrap())))
+        }
+
         return Ok(());
     }
 }
@@ -79,11 +89,10 @@ impl Cake {
 pub enum Host {
     Clr,
     CoreClr,
-    Mono
+    Mono,
 }
 
 pub fn install(config: &Config) -> Result<Cake, Error> {
-
     // Get the version we're going to use.
     let mut version = config.cake_version.clone();
     if version == "latest" {
@@ -99,15 +108,22 @@ pub fn install(config: &Config) -> Result<Cake, Error> {
     }
 
     // Install Cake.
-    let cake_folder_path = config.tools.join(format!("{0}.{1}", flavor.to_lowercase(), version));
+    let cake_folder_path = config
+        .tools
+        .join(format!("{0}.{1}", flavor.to_lowercase(), version));
     if !cake_folder_path.exists() {
-
         let cake_nupkg_path = config.tools.join(get_cake_package_name(&config, &version));
         if !cake_nupkg_path.exists() {
             println!("Downloading {0} {1}...", flavor, version);
-            let url = &format!("https://www.nuget.org/api/v2/package/{0}/{1}", flavor, version);
-            http::download(&url, &cake_nupkg_path,
-                Some(&format!("Cakeup NuGet Client/{0}", ::utils::version::VERSION)[..]))?;
+            let url = &format!(
+                "https://www.nuget.org/api/v2/package/{0}/{1}",
+                flavor, version
+            );
+            http::download(
+                &url,
+                &cake_nupkg_path,
+                Some(&format!("Cakeup NuGet Client/{0}", ::utils::version::VERSION)[..]),
+            )?;
         }
 
         // Nupkg files are just zip files, so unzip it.
@@ -136,7 +152,7 @@ pub fn install(config: &Config) -> Result<Cake, Error> {
     return Ok(Cake {
         path: cake_folder_path.join(&cake_filename),
         version: Version::parse(&version).unwrap(),
-        host: host
+        host: host,
     });
 }
 
