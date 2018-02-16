a84f44b2c4c5bdf892c4d29b4d352f19674e8070
Patrik Svensson <patrik@patriksvensson.se>
2018-02-16 09:18:25 +0100

    Dog food Cakeup when building Cakeup.

--- build.ps1
+++ build.ps1
@@ -1,21 +1,20 @@
-# Fix up the script root.
 if(!$PSScriptRoot){
     $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 }
 
 # Make sure that cakeup is present.
-$CakeUp = Join-Path $PSScriptRoot "cakeup.exe"
-if (!(Test-Path $CakeUp)) {
-    Write-Verbose -Message "Downloading cakeup.exe..."
+$Cakeup = Join-Path $PSScriptRoot "cakeup-x86_64-latest.exe"
+if (!(Test-Path $Cakeup)) {
+    Write-Verbose -Message "Downloading cakeup.exe ($CakeupVersion)..."
     try {        
         $wc = (New-Object System.Net.WebClient);
-        $wc.DownloadFile("https://github.com/patriksvensson/example/releases/download/v0.4.0/cakeup.exe", $CakeUp) } catch {
+        $wc.DownloadFile("https://cakeup.blob.core.windows.net/windows/cakeup-x86_64-latest.exe", $Cakeup) } catch {
             Throw "Could not download cakeup.exe."
     }
 }
 
 # Execute Cakeup
-&$CakeUp "--cake=0.25.0" "--nuget=latest" `
+&$Cakeup "--cake=0.25.0" "--nuget=latest" `
          "--sdk=1.1.7" "--coreclr" `
          "--bootstrap" "--execute" "--" "$args"
 
