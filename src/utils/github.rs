329c1cdc6f7ca0baefbd2235ce41aee81e1d9d0e
Patrik Svensson <patrik@patriksvensson.se>
2018-06-04 12:59:52 +0200

    Return error when downloading goes wrong instead of unwrapping.

--- src/utils/github.rs
+++ src/utils/github.rs
@@ -15,7 +15,7 @@ pub struct GitHubRelease {
 
 pub fn get_latest_release<'a>(owner: &str, repo: &str) -> Result<GitHubRelease, Error> {
     let url = format!("https://api.github.com/repos/{}/{}/releases", owner, repo);
-    let json = http::get(&url, Some("cakeup"));
+    let json = http::get(&url, Some("cakeup"))?;
     let releases : Vec<GitHubRelease> = serde_json::from_str(&json)?;
     return Ok(releases.first().unwrap().clone());
 }
--- src/utils/http.rs
+++ src/utils/http.rs
@@ -7,51 +7,50 @@ extern crate curl;
 use std::str;
 use std::path::Path;
 use self::curl::easy::Easy;
-use std::io::*;
+use std::io::{Write, Error, ErrorKind};
 use std::fs;
 use std::fs::File;
 
-pub fn get(uri: &String, user_agent: Option<&str>) -> String {
-    let mut handle = Easy::new();
-    handle.follow_location(true).unwrap(); // Follow redirects.
-    handle.url(uri).unwrap(); // Set the URL.
-
-    // Add user agent?
-    let user_agent = Option::from(user_agent).unwrap_or("");
-    if !user_agent.is_empty() {
-        let mut list = curl::easy::List::new();
-        list.append(&format!("User-Agent: {}", user_agent)[..]).unwrap();
-        handle.http_headers(list).unwrap();
-    }
-
-    let mut content: String = String::new();
-    {
-        let mut transfer = handle.transfer();
-        transfer
-            .write_function(|data| {
-                content.push_str(str::from_utf8(data).unwrap());
-                Ok(data.len())
-            })
-            .unwrap();
-
-        transfer.perform().unwrap();
-    }
-
-    return content;
-}
-
-pub fn download(uri: &String, path: &Path, user_agent: Option<&str>) -> Result<()> {
+pub fn get(uri: &String, user_agent: Option<&str>) -> Result<String, Error> {
     let mut handle = Easy::new();
     handle.follow_location(true)?; // Follow redirects.
     handle.url(uri)?; // Set the URL.
 
     // Add user agent?
-    let user_agent = Option::from(user_agent).unwrap_or("");
-    if !user_agent.is_empty() {
+    if let Some(agent_name) = user_agent {
         let mut list = curl::easy::List::new();
-        list.append(&format!("User-Agent: {}", user_agent)[..]).unwrap();
-        handle.http_headers(list).unwrap();
+        list.append(&format!("User-Agent: {}", agent_name)[..])?;
+        handle.http_headers(list)?;
     }
+
+    let mut content: String = String::new();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|data| {
+            content.push_str(str::from_utf8(data).unwrap());
+            Ok(data.len())
+        })?;
+
+        transfer.perform()?;
+    }
+
+    return Ok(content);
+}
+
+pub fn download(uri: &String, path: &Path, user_agent: Option<&str>) -> Result<(), Error> {
+    let mut handle = Easy::new();
+    handle.follow_location(true)?; // Follow redirects.
+    handle.url(uri)?; // Set the URL.
+
+    // Add user agent?
+    match user_agent {
+        None => { },
+        Some(ref agent_name) => {
+            let mut list = curl::easy::List::new();
+            list.append(&format!("User-Agent: {}", agent_name)[..])?;
+            handle.http_headers(list)?;
+        }
+    };
 
     // Download the file.
     let mut file = File::create(path)?;
