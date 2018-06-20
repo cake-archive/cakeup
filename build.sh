674febbed2dd1644a949591a657977971192c1e8
Manuel Blanquett <manuel.blanquett@engelbert-strauss.de>
2018-06-20 13:47:35 +0200

    bootstrapper: Pin cakeup version
    Due to the possibility of a breaking change in the latest version or even
    a bug a good practice should be to also pin the `cakeup` tool version.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@
 # Define directories.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/tools
-CAKE_VERSION=0.26.1
+CAKE_VERSION=0.28.1
 CAKE_EXE=$TOOLS_DIR/Cake.CoreClr.$CAKE_VERSION/Cake.dll
 
 # Temporarily skip verification of addins.
